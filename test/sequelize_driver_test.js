8db9b5b7c97cc5a7ac1ed6c11ee4caa7c71fd972
Taka Okunishi <okunishinishi@gmail.com>
2017-07-13 20:32:51 +0900

    Queryの発行を減らした

--- test/sequelize_driver_test.js
+++ test/sequelize_driver_test.js
@@ -332,8 +332,8 @@ describe('sequelize-driver', function () {
     })
     yield driver.drop('Box')
 
-    const NUMBER_OF_ENTITY = 10
-    const NUMBER_OF_ATTRIBUTE = 20
+    const NUMBER_OF_ENTITY = 100
+    const NUMBER_OF_ATTRIBUTE = 10
     let ids = []
 
     // Create
@@ -415,8 +415,8 @@ describe('sequelize-driver', function () {
     })
     yield driver.drop('Box')
 
-    const NUMBER_OF_ENTITY = 100
-    const NUMBER_OF_ATTRIBUTE = 20
+    const NUMBER_OF_ENTITY = 1000
+    const NUMBER_OF_ATTRIBUTE = 10
     let ids = []
 
     // Create
