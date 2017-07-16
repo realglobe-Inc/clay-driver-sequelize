6dcbbb1a228ccf9bd025e9a2e3ce7538c3f21874
rg-ci <ci@realglobe.jp>
2017-07-16 01:35:24 +0000

    [ci skip] Travis CI committed after build

---
+++ test/attribute_model_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for attributeModel.
+ * Runs with mocha.
+ */
+'use strict'
+
+const attributeModel = require('../lib/modeling/attribute_model.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('attribute-model', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Attribute model', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
