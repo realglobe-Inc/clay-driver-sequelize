0860a4d4e23a85972962355f644c21bd78f3e9be
okunishinishi <okunishinishi@gmail.com>
2017-11-11 16:59:39 +0900

    Add locks

---
+++ test/lock_model_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for lockModel.
+ * Runs with mocha.
+ */
+'use strict'
+
+const lockModel = require('../lib/modeling/lock_model.js')
+const assert = require('assert')
+
+describe('lock-model', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Lock model', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
